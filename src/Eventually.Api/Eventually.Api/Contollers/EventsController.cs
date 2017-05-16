2ec8d7cd205b17e895d758ce08cabdda0adbf580
s77848 <s77848@B5982035>
2017-05-16 13:45:45 +0200

    Add Post.

--- src/Eventually.Api/Eventually.Api/Contollers/EventsController.cs
+++ src/Eventually.Api/Eventually.Api/Contollers/EventsController.cs
@@ -5,16 +5,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Eventually.Api.Contollers
-{
-    [Route("api/events")]
-    public class EventsController : Controller
-    {
-        [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            var _event = new { EventId = 2 };
-            return Ok(_event);
-        }
-    }
+namespace Eventually.Api.Contollers {
+	[Route("api/events")]
+	public class EventsController : Controller {
+		[HttpGet]
+		public async Task<IActionResult> Get() {
+			var _event = new { EventId = 2 };
+			return Ok(_event);
+		}
+
+		[HttpGet]
+		[Route("{id}")]
+		public async Task<IActionResult> GetById(int id) {
+			var idToReturn = id;
+			var _event = new { EventId = idToReturn };
+			return Ok(_event);
+		}
+
+		[HttpPost]
+		[Route("{id}")]
+		public async Task<IActionResult> Add(int id) {
+			var idToReturn = id;
+			var _event = new { EventId = idToReturn };
+			return Ok(_event);
+		}
+
+
+	}
 }
