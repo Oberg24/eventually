002cb056750fbbf4d53280209b6724a8ef654831
naslund <naaaslund@gmail.com>
2017-05-16 13:34:55 +0200

    Added controller and startup logic

---
+++ src/Eventually.Api/Eventually.Api/Contollers/EventsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eventually.Api.Contollers
+{
+    [Route("api/events")]
+    public class EventsController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var _event = new { EventId = 2 };
+            return Ok(_event);
+        }
+    }
+}
