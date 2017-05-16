dfe07947ef41e3322ada95d0067ab124589cee1b
Niklas Näslund <naaaslund@gmail.com>
2017-05-17 00:46:23 +0200

    Added users controller, imageurl on event

---
+++ src/Eventually.Api/Eventually.Api/Contollers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Eventually.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eventually.Api.Contollers
+{
+    [Route("api/users")]
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (var context = new EventuallyContext())
+            {
+                return Ok(context.Users.ToList());
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            using (var context = new EventuallyContext())
+            {
+                var user = await context.Users.FindAsync(id);
+                return Ok(user);
+            }
+        }
+    }
+}
