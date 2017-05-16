96fdb23467dd34bd4c10b273e3d0cfacceb76089
naslund <naaaslund@gmail.com>
2017-05-16 13:58:59 +0200

    Added datamodel

---
+++ src/Eventually.Api/Eventually.Api/Models/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eventually.Api.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public virtual IEnumerable<Tag> Tags { get; set; }
+        public virtual IEnumerable<User> Participants { get; set; }
+        public virtual User Creator { get; set; }
+    }
+}
---
+++ src/Eventually.Api/Eventually.Api/Models/Rating.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eventually.Api.Models
+{
+    public class Rating
+    {
+        public int Stars { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Event Event { get; set; }
+    }
+}
