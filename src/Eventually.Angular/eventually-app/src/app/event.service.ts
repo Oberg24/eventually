56c9ef4077e2ee7c2bef5d4c2c83d2db98dc5cb6
Niklas Näslund <naaaslund@gmail.com>
2017-05-17 01:04:49 +0200

    Updated event service

--- src/Eventually.Angular/eventually-app/src/app/event.service.ts
+++ src/Eventually.Angular/eventually-app/src/app/event.service.ts
@@ -1,10 +1,24 @@
-import { Injectable } from '@angular/core';
+﻿import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 import { Event } from './event';
 
 @Injectable()
 export class EventService {
-    getEvents(): Promise<Event[]> {
-        return Promise.resolve(EVENTS);
+    private eventsUrl = 'http://home.naslund.io:5000/api/events';
+
+    constructor(private http: Http) { }
+
+    getEvents(): Observable<Event[]> {
+        return this.http.get(this.eventsUrl).map(this.extractData);
+    }
+
+    private extractData(res: Response) {
+        let body = res.json();
+        return body.data || {};
     }
 }
