6d2cfcaf96bb1423c89ccb05c8ea8252041102af
s77848 <s77848@B5982035>
2017-05-16 14:23:08 +0200

    db

---
+++ src/Eventually.Api/Eventually.Api/Models/EventuallyContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eventually.Api.Models {
+	public class EventuallyContext : DbContext {
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+			optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Eventually;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+		}
+
+		public DbSet<Event> Events { get; set; }
+		public DbSet<Rating> Ratings { get; set; }
+		public DbSet<Tag> Tags { get; set; }
+		public DbSet<User> Users { get; set; }
+
+	}
+}
